34b863e54190b10b519c2407e17a59a23e939ec0
theKorax <zikoramadin@gmail.com>
2023-09-12 13:01:10 +0100

    Troubleshooting
    "using TMPro" is not working for me

--- Lesson5_Clicky/Assets/Scripts/GameManager.cs
+++ Lesson5_Clicky/Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    public TextMesh
     // Start is called before the first frame update
     void Start()
     {
