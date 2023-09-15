86b69f1b5452a0c0db2952b2354e5c75781f0a34
theKorax <zikoramadin@gmail.com>
2023-09-15 17:30:32 +0100

    Added DifficultyButton.cs
    Updated the script to write a message to the console stating the name of the button that was clicked

---
+++ Lesson5_Clicky/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + "was clicked");
+    }
+}
