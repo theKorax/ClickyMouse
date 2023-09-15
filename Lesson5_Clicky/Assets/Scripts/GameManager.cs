37687ecdecd0f92c178a35e3fa681f810137243d
theKorax <zikoramadin@gmail.com>
2023-09-15 04:54:01 +0100

    Updated GameManager.cs
    Added a GameOver() function to set the gameovertext object to true whenever it is called

--- Lesson5_Clicky/Assets/Scripts/GameManager.cs
+++ Lesson5_Clicky/Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     private int score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,5 +38,10 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 }
